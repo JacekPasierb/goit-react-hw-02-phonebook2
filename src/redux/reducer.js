0f21f559c5625fbfeae73545fd012d9410f465d8
Jacek Pasierb <jack_byk@o2.pl>
2023-12-15 20:06:27 +0100

    s

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -5,9 +5,10 @@ const contactsInitialState = [];
 
 export const contactsReducer = createReducer(contactsInitialState, {
   [addContact]: (state, action) => {
-    return [...state, action.payload];
+    state.push(action.payload)
   },
   [deleteContact]: (state, action) => {
-    return state.filter((contact) => contact.id !== action.payload);
+    const index = state.findIndex((contact) => contact.id === action.payload);
+    state.splice(index, 1);
   },
 });
